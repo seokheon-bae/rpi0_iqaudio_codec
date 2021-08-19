7d56b0a8faf2f2e8d604deeee57855af9e539f2e
Seokheon Bae <seokheon.bae@gmail.com>
2021-08-19 18:03:40 +0900

    Adapt the mix.exs to new metadata.

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi0.MixProject do
+defmodule Rpi0IqaudioCodec.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi0
+  @github_organization "seokheon-bae"
+  @app :rpi0_iqaudio_codec
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
